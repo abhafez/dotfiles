ba8a533332781362c8486357438c4347403c847e
Abdurrahman Hafez <abdurrahman.hafez@gmail.com>
2024-03-15 13:34:39 +0200

    feat: keys

--- config.h
+++ config.h
@@ -82,7 +82,7 @@ static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
